5da590af3f2ec505f9a7fc45addbcf6e6c512d03
aowenxuan <aowx21@outlook.com>
2024-06-20 17:13:40 +0800

    update DBRecorder

--- python/src/moss/export.py
+++ python/src/moss/export.py
@@ -120,83 +120,139 @@ class DBRecorder:
             max_lon,  max_lat = proj(x2, y2, True)
         with psycopg2.connect(db_url) as conn:
             with conn.cursor() as cur:
-                cur.execute(f"""
-CREATE TABLE IF NOT EXISTS public.meta_simple (
-    "name" text NOT NULL,
-    "start" int4 NOT NULL,
-    steps int4 NOT NULL,
-    "time" float8 NOT NULL,
-    total_agents int4 NOT NULL,
-    "map" text NOT NULL,
-    min_lng float8 NOT NULL,
-    min_lat float8 NOT NULL,
-    max_lng float8 NOT NULL,
-    max_lat float8 NOT NULL,
-    road_status_v_min float8 NULL,
-    road_status_interval int4 NULL,
-    CONSTRAINT meta_simple_pkey PRIMARY KEY (name)
-);
-DELETE FROM public.meta_simple WHERE name='{output_name}';
-INSERT INTO public.meta_simple VALUES ('{output_name}', {self.eng.start_step}, {len(self.data)}, 1, 1, '{mongo_map}', {min_lon}, {min_lat}, {max_lon}, {max_lat}, 0, 300);
-DROP TABLE IF EXISTS {output_name}_s_cars;
-DROP TABLE IF EXISTS {output_name}_s_people;
-DROP TABLE IF EXISTS {output_name}_s_traffic_light;
-DROP TABLE IF EXISTS {output_name}_s_road;
-CREATE TABLE {output_name}_s_cars (
-    step int4 NOT NULL,
-    id int4 NOT NULL,
-    parent_id int4 NOT NULL,
-    direction float8 NOT NULL,
-    lng float8 NOT NULL,
-    lat float8 NOT NULL,
-    model text NOT NULL,
-    z float8 NOT NULL,
-    pitch float8 NOT NULL,
-    v float8 NOT NULL
-);
-CREATE INDEX {output_name}_s_cars_step_lng_lat_idx ON {output_name}_s_cars USING btree (step, lng, lat);
-CREATE TABLE {output_name}_s_people (
-	step int4 NOT NULL,
-	id int4 NOT NULL,
-	parent_id int4 NOT NULL,
-	direction float8 NOT NULL,
-	lng float8 NOT NULL,
-	lat float8 NOT NULL,
-	z float8 NOT NULL,
-	v float8 NOT NULL,
-	model text NOT NULL
-);
-CREATE INDEX {output_name}_s_people_step_lng_lat_idx ON {output_name}_s_people USING btree (step, lng, lat);
-CREATE TABLE {output_name}_s_traffic_light (
-	step int4 NOT NULL,
-	id int4 NOT NULL,
-	state int4 NOT NULL,
-	lng float8 NOT NULL,
-	lat float8 NOT NULL
-);
-CREATE INDEX {output_name}_s_traffic_light_step_lng_lat_idx ON {output_name}_s_traffic_light USING btree (step, lng, lat);
-CREATE TABLE {output_name}_s_road (
-	step int4 NOT NULL,
-	id int4 NOT NULL,
-	"level" int4 NOT NULL,
-	v float8 NOT NULL,
-	in_vehicle_cnt int4 NOT NULL,
-	out_vehicle_cnt int4 NOT NULL,
-	cnt int4 NOT NULL
-);
-CREATE INDEX {output_name}_s_road_step_idx ON {output_name}_s_road USING btree (step);
-""")
+                # 创建表格
+                cur.execute("""
+                CREATE TABLE IF NOT EXISTS public.meta_simple (
+                    "name" text NOT NULL,
+                    "start" int4 NOT NULL,
+                    steps int4 NOT NULL,
+                    "time" float8 NOT NULL,
+                    total_agents int4 NOT NULL,
+                    "map" text NOT NULL,
+                    min_lng float8 NOT NULL,
+                    min_lat float8 NOT NULL,
+                    max_lng float8 NOT NULL,
+                    max_lat float8 NOT NULL,
+                    road_status_v_min float8 NULL,
+                    road_status_interval int4 NULL,
+                    CONSTRAINT meta_simple_pkey PRIMARY KEY (name)
+                );
+                """)
+                conn.commit()
+
+                # 删除指定记录
+                cur.execute(f"DELETE FROM public.meta_simple WHERE name='{output_name}';")
+                conn.commit()
+
+                # 插入新记录
+                cur.execute(
+                    f"INSERT INTO public.meta_simple VALUES ('{output_name}', {self.eng.start_step}, {len(self.data)}, 1, 1, '{mongo_map}', {min_lon}, {min_lat}, {max_lon}, {max_lat}, 0, 300);")
+                conn.commit()
+
+                # 删除表格
+                cur.execute(f"DROP TABLE IF EXISTS public.{output_name}_s_cars;")
+                cur.execute(f"DROP TABLE IF EXISTS public.{output_name}_s_people;")
+                cur.execute(
+                    f"DROP TABLE IF EXISTS public.{output_name}_s_traffic_light;"
+                )
+                cur.execute(f"DROP TABLE IF EXISTS public.{output_name}_s_road;")
+                conn.commit()
+
+                # 创建表格
+                cur.execute(
+                    f"""
+                CREATE TABLE public.{output_name}_s_cars (
+                    step int4 NOT NULL,
+                    id int4 NOT NULL,
+                    parent_id int4 NOT NULL,
+                    direction float8 NOT NULL,
+                    lng float8 NOT NULL,
+                    lat float8 NOT NULL,
+                    model text NOT NULL,
+                    z float8 NOT NULL,
+                    pitch float8 NOT NULL,
+                    v float8 NOT NULL
+                );
+                """
+                )
+                cur.execute(
+                    f"CREATE INDEX {output_name}_s_cars_step_lng_lat_idx ON public.{output_name}_s_cars USING btree (step, lng, lat);"
+                )
+                conn.commit()
+
+                # 创建表格
+                cur.execute(
+                    f"""
+                CREATE TABLE public.{output_name}_s_people (
+                    step int4 NOT NULL,
+                    id int4 NOT NULL,
+                    parent_id int4 NOT NULL,
+                    direction float8 NOT NULL,
+                    lng float8 NOT NULL,
+                    lat float8 NOT NULL,
+                    z float8 NOT NULL,
+                    v float8 NOT NULL,
+                    model text NOT NULL
+                );
+                """
+                )
+                cur.execute(
+                    f"CREATE INDEX {output_name}_s_people_step_lng_lat_idx ON public.{output_name}_s_people USING btree (step, lng, lat);"
+                )
+                conn.commit()
+
+                # 创建表格
+                cur.execute(
+                    f"""
+                CREATE TABLE public.{output_name}_s_traffic_light (
+                    step int4 NOT NULL,
+                    id int4 NOT NULL,
+                    state int4 NOT NULL,
+                    lng float8 NOT NULL,
+                    lat float8 NOT NULL
+                );
+                """
+                )
+                cur.execute(
+                    f"CREATE INDEX {output_name}_s_traffic_light_step_lng_lat_idx ON public.{output_name}_s_traffic_light USING btree (step, lng, lat);"
+                )
+                conn.commit()
+
+                # 创建表格
+                cur.execute(
+                    f"""
+                CREATE TABLE public.{output_name}_s_road (
+                    step int4 NOT NULL,
+                    id int4 NOT NULL,
+                    "level" int4 NOT NULL,
+                    v float8 NOT NULL,
+                    in_vehicle_cnt int4 NOT NULL,
+                    out_vehicle_cnt int4 NOT NULL,
+                    cnt int4 NOT NULL
+                );
+                """
+                )
+                cur.execute(
+                    f"CREATE INDEX {output_name}_s_road_step_idx ON public.{output_name}_s_road USING btree (step);"
+                )
+                conn.commit()
+
                 cur.copy_from(
                     StringIteratorIO(
                         f"{step},{p},{l},{round(d,3)},{x},{y},'',0,0,{round(v,3)}\n"
-                        for step, vs, x, y in tqdm(vehs, ncols=90, disable=not use_tqdm) for (p, l, d, v), x, y in zip(vs, x, y)
+                        for step, vs, x, y in tqdm(vehs, ncols=90, disable=not use_tqdm)
+                        for (p, l, d, v), x, y in zip(vs, x, y)
                     ),
-                    f'{output_name}_s_cars', sep=','
+                    f"{output_name}_s_cars",
+                    sep=",",
                 )
                 cur.copy_from(
                     StringIteratorIO(
                         f"{step},{p},{s},{x},{y}\n"
-                        for step, ts, x, y in tqdm(tls, ncols=90, disable=not use_tqdm) for (p, s), x, y in zip(ts, x, y)
+                        for step, ts, x, y in tqdm(tls, ncols=90, disable=not use_tqdm)
+                        for (p, s), x, y in zip(ts, x, y)
                     ),
-                    f'{output_name}_s_traffic_light', sep=','
+                    f"{output_name}_s_traffic_light",
+                    sep=",",
                 )
+                conn.commit()
